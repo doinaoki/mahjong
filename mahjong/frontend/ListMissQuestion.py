40c1076f93b6169ebe16788fadca3ce4f8393f59
nndoi <119909153+doinaoki@users.noreply.github.com>
2023-07-10 01:18:12 +0900

    implement show Misslist

--- mahjong/frontend/ListMissQuestion.py
+++ mahjong/frontend/ListMissQuestion.py
@@ -6,11 +6,59 @@ from . import Home
 import ast
 
 class ListMissQuestion:
+
+    #field
+    #now_page_number = 現在表示のページ数
+    #miss_list = ミスった問題集
+    #canvas
+    #root
+
+    def page_button_clicked(self, event):
+        self.now_page_number = event.widget.cget("text")
+        self.canvas.delete("all")
+        self.show_piece()
+
+
+    def show_page(self):
+        self.page_buttons = []
+        miss_list_size = len(self.miss_list)
+        miss_pages = miss_list_size // 5 + 1
+        for i in range(miss_pages):
+            button = tk.Button(
+                self.root,
+                text = i+1,
+                font=("MSゴシック", "20", "bold"),
+            )
+            button.bind("<ButtonPress>", self.page_button_clicked)
+            button.place(
+                x = i*60 + 60, y = 530,
+                width = 50,
+                height = 50)
+            self.page_buttons.append(button)
+    
+        return
+
+    def show_piece(self):
+        s = ["pin1", "pin2", "pin3", "pin4", "pin5", "pin6", "pin7", "pin8", "pin9"]
+        for c in range((self.now_page_number-1)*5, min(len(self.miss_list), (self.now_page_number)*5)):
+            question = self.miss_list[c][0]
+            cc = c % 5
+            t = 1
+            for i in range(len(question)):
+                p = s[i]
+                for k in range(question[i]):
+                    pin = Image.images[p]
+                    self.canvas.create_image(
+                        40+59*t, 100*cc + 60,
+                        image=pin)
+                    t += 1
+        print("show")
+
     def show_list(self, canvas, root):
         with open("../miss_question.txt", 'r' ) as ms:
             miss_questions = ms.read()
         
-        miss_list = []
+        self.miss_list = []
         for i in miss_questions.split("\n"):
             miss = []
             for k in i.split(":"):
@@ -19,8 +67,8 @@ class ListMissQuestion:
                 if k == "Y":
                     miss = []
             if miss != []:
-                miss_list.append(miss)
-        print(miss_list)
+                self.miss_list.append(miss)
+        #print(self.miss_list)
 
         new_canvas = tk.Canvas(
             root,
@@ -31,3 +79,9 @@ class ListMissQuestion:
             highlightthickness = 0,
             relief = "ridge")
         new_canvas.place(x = 0, y = 0)
+
+        self.now_page_number = 1
+        self.canvas = new_canvas
+        self.root = root
+        self.show_page()
+        self.show_piece()
